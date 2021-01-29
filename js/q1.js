4e1d3474ce9e1be9c963a3f7260809e457ec7254
yoshidako <u750186j@outlook.jp>
2021-01-29 18:06:19 +0900

    合計値

--- js/q1.js
+++ js/q1.js
@@ -1,48 +1,45 @@
-
-
-
-
+var sum1 = 0;
+var sum2 = 0;
 
 function culcq1() {
   var elements = document.getElementsByName("q1");
 
   var q1 = "";
-  sum1 = 0;
 
   for (var i = 0; i < elements.length; i++) {
-    console.log(elements[i]);
     if (elements[i].checked) {
       var q1 = elements[i].value;
       break;
     }
   }
-  console.log(q1);
-  document.getElementById("q1write").innerHTML = q1;
-  var sum1 =+ Number( q1 );
-  return sum1;
-}
 
+  document.getElementById("q1write").innerHTML = q1;
+
+  sum1 = +Number(q1);
+  sum();
+}
 
 function culcq2() {
   var elements = document.getElementsByName("q2");
 
   var q2 = "";
-  sum2 = 0;
 
   for (var i = 0; i < elements.length; i++) {
-    console.log(elements[i]);
     if (elements[i].checked) {
       var q2 = elements[i].value;
       break;
     }
   }
-  console.log(q2);
+
   document.getElementById("q2write").innerHTML = q2;
-  var sum2 =+ Number( q2 );
-  return sum2;
+
+  sum2 = +Number(q2);
+  sum();
 }
 
-console.log(culcq1());
-console.log(culcq2());
-var total = culcq1() + culcq2();
-document.getElementById("sum").innerHTML = total;
+function sum() {
+  console.log(sum1, sum2);
+  var total = sum1 + sum2;
+  console.log(total);
+  document.getElementById("sum").innerHTML = total;
+}
