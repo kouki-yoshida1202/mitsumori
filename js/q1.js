a1a386d613a97b8a288e2a536c3249551e79eb41
Hikaru1656 <shining-olaf@outlook.jp>
2021-01-29 17:46:21 +0900

    値段の表示

--- js/q1.js
+++ js/q1.js
@@ -1,17 +1,48 @@
-function culc() {
+
+
+
+
+
+function culcq1() {
   var elements = document.getElementsByName("q1");
 
-  var a = "";
+  var q1 = "";
+  sum1 = 0;
 
   for (var i = 0; i < elements.length; i++) {
     console.log(elements[i]);
     if (elements[i].checked) {
-      var a = elements[i].innerHTML();
+      var q1 = elements[i].value;
       break;
     }
   }
-  console.log(a);
-  var sample = document.getElementById("aiueo").innerHTML;
-  console.log(sample);
-  document.getElementById("q1write").innerHTML = a;
+  console.log(q1);
+  document.getElementById("q1write").innerHTML = q1;
+  var sum1 =+ Number( q1 );
+  return sum1;
 }
+
+
+function culcq2() {
+  var elements = document.getElementsByName("q2");
+
+  var q2 = "";
+  sum2 = 0;
+
+  for (var i = 0; i < elements.length; i++) {
+    console.log(elements[i]);
+    if (elements[i].checked) {
+      var q2 = elements[i].value;
+      break;
+    }
+  }
+  console.log(q2);
+  document.getElementById("q2write").innerHTML = q2;
+  var sum2 =+ Number( q2 );
+  return sum2;
+}
+
+console.log(culcq1());
+console.log(culcq2());
+var total = culcq1() + culcq2();
+document.getElementById("sum").innerHTML = total;
