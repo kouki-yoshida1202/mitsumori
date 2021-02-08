b378965318cc8b74525dfdf5c130792d92697b4e
Hikaru1656 <shining-olaf@outlook.jp>
2021-02-08 15:41:50 +0900

    特定要素のみ表示

--- js/q1.js
+++ js/q1.js
@@ -1,5 +1,10 @@
 var sum1 = 0;
 var sum2 = 0;
+var sum3 = 0;
+var sum4 = 0;
+var sum5 = 0;
+var sum6 = 0;
+var sum7 = 0;
 
 function culcq1() {
   var elements = document.getElementsByName("q1");
@@ -13,6 +18,8 @@ function culcq1() {
     }
   }
 
+  document.getElementById("question1").style.display = "none";
+  document.getElementById("question2").style.display = "block";
   document.getElementById("q1write").innerHTML = q1;
 
   sum1 = +Number(q1);
@@ -31,15 +38,116 @@ function culcq2() {
     }
   }
 
+  document.getElementById("question2").style.display = "none";
+  document.getElementById("question3").style.display = "block";
   document.getElementById("q2write").innerHTML = q2;
 
   sum2 = +Number(q2);
   sum();
 }
 
+function culcq3() {
+  var elements = document.getElementsByName("q3");
+
+  var q3 = "";
+
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].checked) {
+      var q3 = elements[i].value;
+      break;
+    }
+  }
+
+  document.getElementById("question3").style.display = "none";
+  document.getElementById("question4").style.display = "block";
+  document.getElementById("q3write").innerHTML = q3;
+
+  sum3 = +Number(q3);
+  sum();
+}
+
+function culcq4() {
+  var elements = document.getElementsByName("q4");
+
+  var q4 = "";
+
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].checked) {
+      var q4 = elements[i].value;
+      break;
+    }
+  }
+
+  document.getElementById("question4").style.display = "none";
+  document.getElementById("question5").style.display = "block";
+  document.getElementById("q4write").innerHTML = q4;
+
+  sum4 = +Number(q4);
+  sum();
+}
+
+function culcq5() {
+  var elements = document.getElementsByName("q5");
+
+  var q5 = "";
+
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].checked) {
+      var q5 = elements[i].value;
+      break;
+    }
+  }
+
+  document.getElementById("question5").style.display = "none";
+  document.getElementById("question6").style.display = "block";
+  document.getElementById("q5write").innerHTML = q5;
+
+  sum5 = +Number(q5);
+  sum();
+}
+
+function culcq6() {
+  var elements = document.getElementsByName("q6");
+
+  var q6 = "";
+
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].checked) {
+      var q6 = elements[i].value;
+      break;
+    }
+  }
+
+  document.getElementById("question6").style.display = "none";
+  document.getElementById("question7").style.display = "block";
+  document.getElementById("q6write").innerHTML = q6;
+
+  sum6 = +Number(q6);
+  sum();
+}
+
+function culcq7() {
+  var elements = document.getElementsByName("q7");
+
+  var q7 = "";
+
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].checked) {
+      var q7 = elements[i].value;
+      break;
+    }
+  }
+
+  document.getElementById("question7").style.display = "none";
+  document.getElementById("q7write").innerHTML = q7;
+
+  sum1 = +Number(q7);
+  sum();
+}
+
 function sum() {
-  console.log(sum1, sum2);
-  var total = sum1 + sum2;
+  console.log(sum1, sum2, sum3, sum4, sum5, sum6, sum7);
+  var total = sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7;
   console.log(total);
   document.getElementById("sum").innerHTML = total;
 }
