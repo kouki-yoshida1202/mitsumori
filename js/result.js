e083217989a9f48ce98a92088409f3f3d34abb63
yoshidako <u750186j@outlook.jp>
2021-03-04 16:37:52 +0900

    メール送信機能の追加

--- js/result.js
+++ js/result.js
@@ -1,3 +1,9 @@
-var params = (new URL(document.location)).searchParams;
-var total = params.get('total');
+var params = new URL(document.location).searchParams;
+var total = params.get("total");
 document.getElementById("result").innerHTML = total;
+
+$.ajax("../php/result.php", {
+  type: "post",
+  data: { total: total },
+  dataType: "json"
+});
---
+++ php/result.php
@@ -0,0 +1,18 @@
+<?php
+mb_language("Japanese");
+mb_internal_encoding("UTF-8");
+
+$total = $_POST['total'];
+
+$to = "contact@gonmura.info";
+$title = "【見積もりされました】";
+$content = "\r\n新規見積もりが入りました。
+\r\n\r\n============================================================\r\n
+金額 : " . $total . "万円\r\n\r\n============================================================\r\n\r\n───────────────────────────────────\r\n\r\nby 見積もりシミュレーション\r\n\r\n───────────────────────────────────\r\n";
+
+$headers = "From: u750186j@gmail.com";
+$headers .= "\r\n";
+
+mb_send_mail($to, $title, $content, $headers);
+echo "success";
+return;
