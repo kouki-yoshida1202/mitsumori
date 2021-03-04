1f42602abb0ec5022f2fe8f0f7f805a311fb64e0
yoshidako <u750186j@outlook.jp>
2021-03-04 17:26:14 +0900

    問い合わせ完成版

--- js/result.js
+++ js/result.js
@@ -1,6 +1,9 @@
 var params = new URL(document.location).searchParams;
 var total = params.get("total");
 document.getElementById("result").innerHTML = total;
+setTimeout(function() {
+  $("#priceHidden").val(total);
+}, 1000);
 
 $.ajax("../php/result.php", {
   type: "post",
